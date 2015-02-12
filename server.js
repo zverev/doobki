b789be0238a512f2e35eeb937e5da92b4c239cf8
zverev <alexander.zverev@gmail.com>
2015-02-12 21:29:58 +0300

    millisecondstomay

--- server.js
+++ server.js
@@ -6,13 +6,28 @@ var fs = require('fs');
 
 var app = express();
 
-app.use(function(req, res) {
-    res.send(JSON.parse(fs.readFileSync('./config.json')).foo);
+app.use(function(req, res, next) {
+    if (req.url === '/millisecondstomay') {
+        var now = new Date();
+        var may = new Date(now.getFullYear(), 04, 01);
+        var timeLeft = may.getTime() - now.getTime();
+        if (!timeLeft) {
+            res.status(200).send('0');
+        } else {
+            res.status(200).send(timeLeft + '');
+        }
+    } else {
+        next();
+    }
 });
+
+app.use(function(req, res) {
+    res.status(404).send('not found');
+})
 
 console.log('port: ', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 console.log('ip: ', process.env.OPENSHIFT_NODEJS_IP);
 
 http.createServer(app).listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP, function(req, res) {
-    console.log('listening on port ' + process.env.OPENSHIFT_NODEJS_PORT || 8080);
+    console.log('listening on port ' + (process.env.OPENSHIFT_NODEJS_PORT || 8080));
 });
