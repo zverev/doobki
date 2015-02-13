77b28fd8b03d9cd52ff52f0eeab4f6d261320d4f
zverev <alexander.zverev@gmail.com>
2015-02-13 21:28:13 +0300

    pick a background

---
+++ client.js
@@ -0,0 +1,21 @@
+var getRandomInt = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+$(document).ready(function() {
+    $.ajax('config.json').then(function(cfg) {
+        var fb = new Firebase(cfg.backgroundsRef);
+        fb.on('child_added', function(snapshot) {
+            // choose a background
+            var a = [];
+            var o = snapshot.val();
+            for (p in o) {
+                if (o.hasOwnProperty(p)) {
+                    a.push(o[p]);
+                }
+            }
+            var bg = a[getRandomInt(0, a.length-1)];
+            $('body').css('background-image', 'url(' + bg.url + ')');
+        });
+    });
+});
