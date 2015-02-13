1f7dbda3a00b2f0443641c313c904b8d50a06921
zverev <alexander.zverev@gmail.com>
2015-02-13 21:57:51 +0300

    allow cross-domain requests

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var fs = require('fs');
 var app = express();
 
 app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     if (req.url === '/millisecondstomay') {
         var now = new Date();
         var may = new Date(now.getFullYear(), 04, 01);
