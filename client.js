0a975c051b81ce933e2a2cfb6375c7c072c61d46
zverev <alexander.zverev@gmail.com>
2015-02-14 12:09:54 +0300

    refactor Background

--- client.js
+++ client.js
@@ -18,36 +18,62 @@ Countdown.prototype._tick = function() {
     this._$placeholder.html(this._mayTime.diff(this._currentTime, 'seconds'));
 };
 
+// <DOMNode> options.placeholder
+// <Object> options.backgrounds
+var Background = function(options) {
+    this._backgrounds = options.backgrounds;
+    this._$placeholder = $(options.placeholder);
+    this._defaultBlur = options.defaultBlur || 0;
+    this._switch(this._pickRandom(this._backgrounds));
+};
+
+// <Object> backgrounds
+Background.prototype._pickRandom = function(backgrounds) {
+    var a = [];
+    var o = backgrounds;
+    for (p in o) {
+        if (o.hasOwnProperty(p)) {
+            a.push(o[p]);
+        }
+    }
+    return a[getRandomInt(0, a.length - 1)];
+};
+
+// <Object> background
+Background.prototype._switch = function(bg) {
+    var getBlurStyleString = function(blurAmount) {
+        var s = '';
+        var p = ['filter', '-o-filter', '-ms-filter', '-moz-filter', '-webkit-filter'];
+        p.map(function(e) {
+            s += e + ': ' + 'blur(' + blurAmount + 'px' + ')' + ';';
+        });
+        return s;
+    };
+
+    var $bg = this._$placeholder;
+
+    $('<img>').load(function() {
+        // after image loads
+        $bg.css('opacity', 0);
+        $bg.css('background-image', 'url(' + bg.url + ')');
+
+        var blur = bg.blur || this._defaultBlur;
+        $bg[0].setAttribute('style', $bg[0].getAttribute('style') + ' ' + getBlurStyleString(blur));
+
+        $bg.animate({
+            opacity: bg.opacity ? bg.opacity + '' : '1'
+        }, 1500);
+    }).attr('src', bg.url);
+};
+
 $(document).ready(function() {
     $.ajax('config.json').then(function(cfg) {
         var fb = new Firebase(cfg.backgroundsRef);
         fb.on('child_added', function(snapshot) {
-            // choose a background
-            var a = [];
-            var o = snapshot.val();
-            for (p in o) {
-                if (o.hasOwnProperty(p)) {
-                    a.push(o[p]);
-                }
-            }
-            var bg = a[getRandomInt(0, a.length - 1)];
-            $('<img>').load(function() {
-                $('.bg').css('opacity', 0);
-                $('.bg').css('background-image', 'url(' + bg.url + ')');
-                var blur = bg.blur || cfg.defaultBlur || 0;
-                var getBlurStyleString = function(blurAmount) {
-                    var s = '';
-                    var p = ['filter', '-o-filter', '-ms-filter', '-moz-filter', '-webkit-filter'];
-                    p.map(function(e) {
-                        s += e + ': ' + 'blur(' + blurAmount + 'px' + ')' + ';';
-                    });
-                    return s;
-                };
-                $('.bg')[0].setAttribute('style', $('.bg')[0].getAttribute('style') + ' ' + getBlurStyleString(blur));
-                $('.bg').animate({
-                    opacity: bg.opacity ? bg.opacity + '' : '1'
-                }, 1500);
-            }).attr('src', bg.url);
+            new Background({
+                placeholder: $('.background')[0],
+                backgrounds: snapshot.val()
+            });
         });
         $.ajax(cfg.timeUrl).then(function(response) {
             new Countdown({
