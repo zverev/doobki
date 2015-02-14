214c8c29d6ccb04df8d4a4b8971e191a486f0cb4
zverev <alexander.zverev@gmail.com>
2015-02-14 10:59:16 +0300

    add some blur, shadows, etc.

--- client.js
+++ client.js
@@ -18,8 +18,19 @@ $(document).ready(function() {
             $('<img>').load(function() {
                 $('.bg').css('opacity', 0);
                 $('.bg').css('background-image', 'url(' + bg.url + ')');
+                console.log(bg.blur)
+                var blur = bg.blur || cfg.defaultBlur || 0;
+                var getBlurStyleString = function(blurAmount) {
+                    var s = '';
+                    var p = ['filter', '-o-filter', '-ms-filter', '-moz-filter', '-webkit-filter'];
+                    p.map(function(e) {
+                        s += e + ': ' + 'blur(' + blurAmount + 'px' + ')' + ';';
+                    });
+                    return s;
+                };
+                $('.bg')[0].setAttribute('style',  $('.bg')[0].getAttribute('style') + ' ' + getBlurStyleString(blur));
                 $('.bg').animate({
-                    opacity: '1'
+                    opacity: bg.opacity ? bg.opacity + '' : '1'
                 }, 1500);
             }).attr('src', bg.url);
         });
